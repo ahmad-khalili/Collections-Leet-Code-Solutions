7142c81b0f0970ecb3861fa299971651a579d1b3
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-06 17:21:11 +0300

    Add Valid Anagram solution

--- Collections-Leet-Code/Program.cs
+++ Collections-Leet-Code/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace LeetCode.Collections;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        int alphabet = 52;
+        int[] temp = new int[alphabet];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            temp[Math.Abs(s[i] - 'a')]++;
+            temp[Math.Abs(t[i] - 'a')]--;
+        }
+        
+        foreach(var number in temp)
+            if (number != 0)
+                return false;
+
+        return true;
+    }
+    static void Main()
+    {
+        Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        Console.WriteLine(IsAnagram("Anagram", "nagaram"));
+        Console.WriteLine(IsAnagram("rat", "car"));
+    }
+}
