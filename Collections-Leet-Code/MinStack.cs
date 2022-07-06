faff0444ce89dbc98780952018eff252d6915eb8
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-06 12:56:29 +0300

    Add Min Stack solution

---
+++ Collections-Leet-Code/MinStack.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Collections;
+
+public class MinStack
+{
+    private Stack<int> _stack;
+    private Stack<int> _minStack;
+    
+    public MinStack()
+    {
+        _stack = new();
+        _minStack = new();
+    }
+
+    public void Push(int item)
+    {
+        if (_minStack.Count == 0 || item < _minStack.Peek())
+        {
+            _minStack.Push(item);
+        }
+        _stack.Push(item);
+    }
+
+    public int Pop()
+    {
+        if (_minStack.Peek() == _stack.Peek())
+        {
+            _minStack.Pop();
+        }
+
+        return _stack.Pop();
+    }
+
+    public int Top()
+    {
+        return _stack.Peek();
+    }
+
+    public int GetMin()
+    {
+        return _minStack.Peek();
+    }
+}
