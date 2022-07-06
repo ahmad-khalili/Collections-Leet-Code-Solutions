116213e011569a3e9fc91ab0460939f60ec539d5
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-06 15:36:12 +0300

    Add Watering Plants solution

--- Collections-Leet-Code/Program.cs
+++ Collections-Leet-Code/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace LeetCode.Collections;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static int WateringPlants(int[] plants, int capacity) {
+        int steps = 0;
+        int cap = capacity;
+        int i = 0;
+        while (i < plants.Length)
+        {
+            if (plants[i] > cap)
+            {
+                int j = i;
+                while (j > 0)
+                {
+                    j -= 1;
+                    steps += 1;
+                }
+
+                cap = capacity;
+
+                while (j < i)
+                {
+                    j += 1;
+                    steps += 1;
+                }
+                
+                cap -= plants[i];
+                i++;
+                steps++;
+            }
+            else
+            {
+                cap -= plants[i];
+                i += 1;
+                steps += 1;
+            }
+        }
+
+        return steps;
+    }
+    
+    static void Main()
+    {
+        int[] plants = new[] { 2, 2, 3, 3 };
+        int capacity = 5;
+        var steps = WateringPlants(plants, capacity);
+        Console.WriteLine($"Total number of steps are: {steps}");
+    }
+}
